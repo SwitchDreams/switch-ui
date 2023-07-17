ab38ee3382bc37fb6c0740dba00dbe76d1e99af2
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-07-17 11:57:01 -0300

    Adding tests setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,41 @@
-import { resolve } from 'node:path'
+import { resolve } from "node:path"
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import EsLint from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+import EsLint from "vite-plugin-linter"
+import tsConfigPaths from "vite-tsconfig-paths"
+
 const { EsLinter, linterPlugin } = EsLint
-import * as packageJson from './package.json'
+import * as packageJson from "./package.json"
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [
     react(),
     tsConfigPaths(),
     linterPlugin({
-      include: ['./src}/**/*.{ts,tsx}'],
+      include: ["./src}/**/*.{ts,tsx}"],
       linters: [new EsLinter({ configEnv })],
     }),
     dts({
-      include: ['src/component/'],
+      include: ["src/component/"],
     }),
   ],
   build: {
     lib: {
-      entry: resolve('src', 'components/index.ts'),
-      name: 'ReactViteLibrary',
-      formats: ['es', 'umd'],
+      entry: resolve("src", "components/index.ts"),
+      name: "ReactViteLibrary",
+      formats: ["es", "umd"],
       fileName: (format) => `react-vite-library.${format}.js`,
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+    testMatch: ["./tests/**/*.test.tsx"],
+    globals: true,
   },
 }))
