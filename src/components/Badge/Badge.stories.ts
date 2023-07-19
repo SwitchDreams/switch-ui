0dff745802144dfac822425ecc683dfe9853224d
VHvictor1 <vitorios20001@gmail.com>
2023-07-19 19:35:17 -0300

    :sparkles: Try the new library

--- src/components/Badge/Badge.stories.ts
+++ src/components/Badge/Badge.stories.ts
@@ -1,20 +1,20 @@
-import type { Meta, StoryObj } from "@storybook/react"
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Badge } from "./Badge"
+import { Badge } from "./Badge";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
   title: "Example/Badge",
   component: Badge,
   tags: ["autodocs"],
-} satisfies Meta<typeof Badge>
+} satisfies Meta<typeof Badge>;
 
-export default meta
-type Story = StoryObj<typeof meta>
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
   args: {
     label: "Hello World",
   },
-}
+};
