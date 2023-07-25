9694e011d785c32e43a1eea25989730617fda3fc
VHvictor1 <vitorios20001@gmail.com>
2023-07-25 09:13:52 -0300

    :sparkles: Component slider
    try to style the slider component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,26 +2,35 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { HTMLProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-const sliderVariants = cva("button", {
-  variants: {
-    size: {
-      small: "px-2 py-1 text-sm",
-      medium: "text-base px-4 py-2",
+const sliderVariants = cva(
+  "w-full appearance-none border-0 accent-primary-300 hover:accent-primary-400 [&::-webkit-slider-thumb]:h-[40px] [&::-webkit-slider-thumb]:w-[40px] [&::-webkit-slider-thumb]:bg-primary-25",
+  {
+    variants: {
+      size: {
+        small: "px-2 py-1 text-sm",
+        medium: "text-base px-4 py-2",
+      },
+    },
+    defaultVariants: {
+      size: "medium",
     },
   },
-  defaultVariants: {
-    size: "medium",
-  },
-});
+);
 
 export interface SliderProps
   extends Omit<HTMLProps<HTMLInputElement>, "size">,
     VariantProps<typeof sliderVariants> {}
 
-export const Slider = ({ size, className, ...rest }: SliderProps) => {
+export const Slider = ({ size, min, max, value, step, className, ...rest }: SliderProps) => {
   return (
-    <div className={twMerge(sliderVariants({ size }), className)} {...rest}>
-      <input type="range"></input>
-    </div>
+    <input
+      type="range"
+      min={min}
+      max={max}
+      value={value}
+      step={step}
+      className={twMerge(sliderVariants({ size }), className)}
+      {...rest}
+    ></input>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-import colors from './src/constants/colors';
+import colors from "./src/constants/colors";
 
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: colors
+      colors: colors,
     },
     fontFamily: {
-      Poppins: ['Poppins', 'sans-serif'],
+      Poppins: ["Poppins", "sans-serif"],
     },
     fontSize: {
-      '7xl': '72px',
-      '6xl': '60px',
-      '5xl': '48px',
-      '4.5xl': '40px',
-      '4xl': '36px',
-      '3xl': '30px',
-      '2xl': '24px',
-      'xl': '20px',
-      'lg': '18px',
-      'md': '16px',
-      'sm': '14px',
-      'xs': '12px',
-    }
+      "7xl": "72px",
+      "6xl": "60px",
+      "5xl": "48px",
+      "4.5xl": "40px",
+      "4xl": "36px",
+      "3xl": "30px",
+      "2xl": "24px",
+      xl: "20px",
+      lg: "18px",
+      md: "16px",
+      sm: "14px",
+      xs: "12px",
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
