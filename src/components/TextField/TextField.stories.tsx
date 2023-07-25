126c4299343518ecbb7eb8a59f8ce253d93adc37
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-07-25 11:26:59 -0300

    segregate forms stories

--- src/components/TextField/TextField.stories.tsx
+++ src/components/TextField/TextField.stories.tsx
@@ -5,7 +5,7 @@ import TextField from "./TextField";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Example/TextField",
+  title: "Forms/TextField",
   component: TextField,
   tags: ["autodocs"],
 } satisfies Meta<typeof TextField>;
