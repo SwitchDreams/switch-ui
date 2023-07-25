da9acdf3781744f019eeb71578ad87a53b6ba931
VHvictor1 <vitorios20001@gmail.com>
2023-07-25 12:06:36 -0300

    :sparkles: Component radio button
    create the component radio button

---
+++ src/components/CheckBox/CheckBox.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { CheckBox } from "./CheckBox";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Example/CheckBox",
+  component: CheckBox,
+  tags: ["autodocs"],
+} satisfies Meta<typeof CheckBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {};
---
+++ src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,47 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { HTMLProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+export interface ICheckBox {
+  disabled?: boolean;
+}
+
+const checkBoxVariants = cva(
+  "relative appearance-none rounded-full border border-gray-200 checked:border-primary-300  checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:rounded-full checked:after:border-primary-300 checked:after:bg-primary-300 checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:border-primary-300 hover:bg-primary-25 focus:ring-4 focus:ring-primary-25",
+  {
+    variants: {
+      size: {
+        small: "h-4 w-4 checked:after:h-[0.4rem] checked:after:w-[0.4rem]",
+        medium: "h-5 w-5 checked:after:h-[0.54rem] checked:after:w-[0.54rem]",
+        large: "h-6 w-6 checked:after:h-[0.6rem] checked:after:w-[0.6rem]",
+      },
+    },
+    defaultVariants: {
+      size: "medium",
+    },
+  },
+);
+
+export interface CheckBoxProps
+  extends Omit<HTMLProps<HTMLInputElement>, "size" | "disabled">,
+    ICheckBox,
+    VariantProps<typeof checkBoxVariants> {}
+
+export const CheckBox = ({ size, disabled = false, className, ...rest }: CheckBoxProps) => {
+  return (
+    <input
+      type="radio"
+      disabled={disabled}
+      className={
+        disabled
+          ? twMerge(
+              checkBoxVariants({ size }),
+              className,
+              "border-gray-200 bg-gray-100 checked:border-gray-200 checked:after:bg-gray-500 hover:border-gray-200 hover:bg-gray-100",
+            )
+          : twMerge(checkBoxVariants({ size }), className)
+      }
+      {...rest}
+    ></input>
+  );
+};
