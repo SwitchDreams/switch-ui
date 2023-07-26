c2d018e94df07ab1e201f13700110aa1d87ab82d
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-07-26 11:46:30 -0300

    fixes prettier

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
+// Export css
 import "../index.css";
+import "../fonts.css";
 
+import colors from "../constants/colors";
 import { Badge } from "./Badge/Badge";
 import Button from "./Buttons/Button";
 import FloatingButton from "./FloatingButton";
@@ -9,4 +12,8 @@ import TextArea from "./TextArea";
 import TextField from "./TextField/TextField";
 import ToggleSwitch from "./ToggleSwitch";
 
+// Export componentes
 export { Badge, Button, FloatingButton, Spinner, Text, TextArea, TextField, ToggleSwitch };
+
+// Export default colors
+export { colors };
