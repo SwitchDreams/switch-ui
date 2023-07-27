2b25baef7abb12984c343792905819961d16359c
VHvictor1 <vitorios20001@gmail.com>
2023-07-27 10:59:53 -0300

    finish the checkbox and radio button

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -32,6 +32,22 @@ const checkBoxVariants = cva("peer appearance-none transition-all duration-200",
   },
 });
 
+const iconVariants = cva(
+  "absolute  z-[-1] font-bold text-white peer-checked:z-10 peer-checked:text-opacity-100",
+  {
+    variants: {
+      size: {
+        small: "left-[0.2rem] top-[0.1rem] h-[10px] w-[10px]",
+        medium: "left-[0.25rem] top-[-0.05rem] h-3 w-3",
+        large: "left-[0.25rem] top-[-0.3rem] h-4 w-4",
+      },
+    },
+    defaultVariants: {
+      size: "medium",
+    },
+  },
+);
+
 export interface CheckBoxProps
   extends Omit<HTMLProps<HTMLInputElement>, "size" | "disabled" | "shape" | "color" | "name">,
     ICheckBox,
@@ -50,18 +66,34 @@ export const CheckBox = ({
     <label className="relative" htmlFor={name}>
       <input
         name={name}
+        aria-label={name}
         type="checkbox"
         disabled={disabled}
-        className={twMerge(checkBoxVariants({ size, shape, color }), className)}
+        className={
+          disabled
+            ? twMerge(
+                checkBoxVariants({ size, shape }),
+                "border-gray-200 bg-gray-100 checked:border-gray-200 checked:after:bg-gray-500 hover:border-gray-200",
+              )
+            : twMerge(checkBoxVariants({ size, shape, color }), className)
+        }
         {...rest}
       ></input>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="2.5"
+        stroke-width="3.5"
         stroke="currentColor"
-        className="absolute top-1/2 z-[-1] h-3 w-3 font-bold text-white peer-checked:z-10 peer-checked:text-opacity-100"
+        className={
+          disabled
+            ? twMerge(
+                iconVariants({ size }),
+                className,
+                "border-gray-200 bg-gray-100 checked:border-gray-200 checked:after:bg-gray-500 hover:border-gray-200 hover:bg-gray-100",
+              )
+            : twMerge(iconVariants({ size }), className)
+        }
       >
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
       </svg>
