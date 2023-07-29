ea232ff70426b8ca84bb5918487047fc6dd4d1db
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-07-29 10:51:53 -0300

    :fire: removes eslint plugin from build

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,14 @@ import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import EsLint from "vite-plugin-linter";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-const { EsLinter, linterPlugin } = EsLint;
 import * as packageJson from "./package.json";
 // https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
+export default defineConfig(() => ({
   plugins: [
     react(),
     tsConfigPaths(),
-    linterPlugin({
-      include: ["./src/**/*.{ts,tsx}"],
-      linters: [new EsLinter({ configEnv })],
-    }),
     dts({
       include: ["src/components/"],
       exclude: ["**/*.test.tsx", "**/*.stories.tsx"],
