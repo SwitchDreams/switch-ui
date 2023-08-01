50bfd4d32731dfe7f98dcc5e803642c6dc65790c
ccarlaa <carlacarlaclementino@gmail.com>
2023-08-01 08:38:21 -0300

    fix tests

--- src/components/AccordionMenu/__tests__/index.test.tsx
+++ src/components/AccordionMenu/__tests__/index.test.tsx
@@ -1,22 +1,22 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 
-import AccordingMenu from "../index";
+import AccordionMenu from "../index";
 
-describe("AccordingMenu", () => {
+describe("AccordionMenu", () => {
   it("test the props", () => {
     const title = "Título de teste";
-    const infos = "Informações de teste";
+    const children = "Informações de teste";
 
-    render(<AccordingMenu title={title} infos={infos} size="md" />);
+    render(<AccordionMenu title={title} children={children} size="md" />);
 
     expect(screen.getByText(title)).toBeInTheDocument();
   });
 
   it("test the title color", () => {
     const title = "Título de teste";
-    const infos = "Informações de teste";
+    const children = "Informações de teste";
 
-    render(<AccordingMenu title={title} infos={infos} size="md" />);
+    render(<AccordionMenu title={title} children={children} size="md" />);
 
     expect(screen.getByText(title)).not.toHaveClass("text-primary-300");
 
@@ -27,48 +27,48 @@ describe("AccordingMenu", () => {
 
   it("test size lg", () => {
     const title = "Título de teste";
-    const infos = "Informações de teste";
+    const children = "Informações de teste";
 
-    render(<AccordingMenu title={title} infos={infos} size="lg" />);
+    render(<AccordionMenu title={title} children={children} size="lg" />);
 
-    const menuButton = document.querySelector(".according-button");
+    const menuButton = document.querySelector(".accordion-button");
     expect(menuButton).toHaveClass("py-5");
     expect(menuButton).toHaveClass("text-lg");
 
     fireEvent.click(screen.getByText(title));
-    const infosPanel = document.querySelector(".according-infos");
+    const infosPanel = document.querySelector(".accordion-infos");
     expect(infosPanel).toHaveClass("pb-6");
     expect(infosPanel).toHaveClass("text-md");
   });
 
   it("test size md", () => {
     const title = "Título de teste";
-    const infos = "Informações de teste";
+    const children = "Informações de teste";
 
-    render(<AccordingMenu title={title} infos={infos} />); // Tamanho md é o padrão
+    render(<AccordionMenu title={title} children={children} />);
 
-    const menuButton = document.querySelector(".according-button");
+    const menuButton = document.querySelector(".accordion-button");
     expect(menuButton).toHaveClass("py-4");
     expect(menuButton).toHaveClass("text-md");
 
     fireEvent.click(screen.getByText(title));
-    const infosPanel = document.querySelector(".according-infos");
+    const infosPanel = document.querySelector(".accordion-infos");
     expect(infosPanel).toHaveClass("pb-5");
     expect(infosPanel).toHaveClass("text-sm");
   });
 
   it("test size sm", () => {
     const title = "Título de teste";
-    const infos = "Informações de teste";
+    const children = "Informações de teste";
 
-    render(<AccordingMenu title={title} infos={infos} size="sm" />);
+    render(<AccordionMenu title={title} children={children} size="sm" />);
 
-    const menuButton = document.querySelector(".according-button");
+    const menuButton = document.querySelector(".accordion-button");
     expect(menuButton).toHaveClass("py-3");
     expect(menuButton).toHaveClass("text-sm");
 
     fireEvent.click(screen.getByText(title));
-    const infosPanel = document.querySelector(".according-infos");
+    const infosPanel = document.querySelector(".accordion-infos");
     expect(infosPanel).toHaveClass("pb-4");
     expect(infosPanel).toHaveClass("text-xs");
   });
