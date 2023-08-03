d33a4d9add693fcd914284de9793f7ebc07249ac
VHvictor1 <vitorios20001@gmail.com>
2023-08-03 12:55:02 -0300

    start to create the component input search

---
+++ src/components/SearchInput/SearchInput.stories.ts
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Text } from "./index";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Others/Text",
+  component: Text,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    children: "Texto padrão",
+    as: "p",
+  },
+};
+
+export const SizeChange: Story = {
+  args: {
+    children: "Texto padrão",
+    as: "p",
+    size: "7xl",
+  },
+};
+
+export const HtmlChange: Story = {
+  args: {
+    children: "Texto padrão",
+    as: "h2",
+  },
+};
+
+export const UserChanges: Story = {
+  args: {
+    children: "Texto padrão",
+    as: "p",
+    size: "2xl",
+    className: "text-orange-100 font-bold",
+  },
+};
