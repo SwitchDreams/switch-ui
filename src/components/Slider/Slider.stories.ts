4ad75a815633209c068816cf04efdc40c6a071f7
VHvictor1 <vitorios20001@gmail.com>
2023-08-10 10:18:33 -0300

    :fire: Remove code errors and unused libs

--- src/components/Slider/Slider.stories.ts
+++ src/components/Slider/Slider.stories.ts
@@ -4,7 +4,7 @@ import { Slider } from "./Slider";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Example/Slider",
+  title: "Forms/Slider",
   component: Slider,
   tags: ["autodocs"],
 } satisfies Meta<typeof Slider>;
