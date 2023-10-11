dcb3cd8afce4cb778c76077a3a79eb133ab83d20
VHvictor1 <vitorios20001@gmail.com>
2023-10-11 11:23:35 -0300

    :bug: Fix the uncontrolled component

---
+++ src/components/SelectBox/SelectBoxState.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { useState } from "react";
+
+import SelectBox from "./index";
+
+const meta = {
+  title: "Forms/SelectBox",
+  component: SelectBox,
+  tags: [],
+} satisfies Meta<typeof SelectBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const options = [
+  {
+    label: "casa1",
+    value: 1,
+  },
+  {
+    label: "casa2",
+    value: 2,
+  },
+  {
+    label: "casa3",
+    value: 3,
+  },
+];
+
+const Template: StoryFn<any> = () => {
+  const [data, setData] = useState();
+  return (
+    <div className="w-full">
+      <SelectBox
+        label="Ambientes"
+        size="lg"
+        placeholder="Escolha o(s) ambiente(s)"
+        options={options}
+        multiple={true}
+        value={data}
+        onChange={(e) => setData(e)}
+      />
+    </div>
+  );
+};
+
+export const WithState: Story = Template;
