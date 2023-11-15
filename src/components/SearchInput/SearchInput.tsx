f66252aa4a36fd4244bd8694e7013b831cb4aa12
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-11-15 10:08:05 -0300

    initialize async search input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,10 +1,11 @@
 import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { cva, type VariantProps } from "class-variance-authority";
-import { InputHTMLAttributes, useState } from "react";
+import { InputHTMLAttributes, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Options = {
-  info: any;
+  label: string;
+  value: any;
 };
 
 interface SearchInputType {
@@ -13,6 +14,7 @@ interface SearchInputType {
   size?: "lg" | "md" | "sm";
   disabled?: boolean;
   selectedValue?: string;
+  apiUrl?: string;
   setSelectedValue: (value: any) => void;
 }
 
@@ -45,17 +47,46 @@ function SearchInput({
   disabled = false,
   className,
   selectedValue = "",
+  apiUrl,
   setSelectedValue,
   ...rest
 }: SearchInputProps) {
   const [query, setQuery] = useState("");
+  const [apiOptions, setApiOptions] = useState<Options[]>([]);
 
-  const filteredOption =
-    query === ""
-      ? options
-      : options.filter((option) => {
-          return option.info.toLowerCase().includes(query.toLowerCase());
-        });
+  useEffect(() => {
+    if (!apiUrl || apiUrl === "") {
+      return;
+    }
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl + `?query=${query}`);
+        const data = await response.json();
+        setApiOptions(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if (query !== "") {
+      fetchData();
+    }
+  }, [query, apiUrl]);
+
+  let filteredOption: Options[] = [];
+
+  // Handles filtering options
+  if (apiUrl) {
+    filteredOption = apiOptions;
+  } else {
+    if (query === "") {
+      filteredOption = options;
+    } else {
+      filteredOption = options.filter((option) =>
+        option.label.toLowerCase().includes(query.toLowerCase()),
+      );
+    }
+  }
 
   const handleOptionClick = (value: string) => {
     setSelectedValue(value);
@@ -79,13 +110,13 @@ function SearchInput({
         <ul className="absolute inset-x-0 top-full mt-1 flex max-h-52 flex-col rounded-lg border border-gray-100 bg-white opacity-0 peer-focus:opacity-100">
           {filteredOption.map((option) => (
             <div
-              key={option.info}
+              key={option.value}
               className="flex h-14 cursor-pointer items-center px-4 text-md text-gray-900 hover:bg-gray-50"
               onClick={() => {
-                handleOptionClick(option.info);
+                handleOptionClick(option.value);
               }}
             >
-              {option.info}
+              {option.label}
             </div>
           ))}
         </ul>
