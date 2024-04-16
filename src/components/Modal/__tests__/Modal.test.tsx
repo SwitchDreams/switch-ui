b8aa92c19c8be8c99b236af2aa4d915554ae4c58
Victor Hugo da Silva de Oliveira <vhvictor1@pop-os.localdomain>
2024-04-16 16:28:02 -0300

    🎨 Make the review changes

--- src/components/Modal/__tests__/Modal.test.tsx
+++ src/components/Modal/__tests__/Modal.test.tsx
@@ -1,28 +1,77 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { vitest } from "vitest";
 
 import Modal from "../index";
+
+// Mocks
+const mockSetOpen = vitest.fn();
+const mockOnClickConfirm = vitest.fn();
+const mockOnClickCancel = vitest.fn();
 
 describe("Modal Component", () => {
   it("renders without crashing", () => {
     render(
-      <Modal open={true} setOpen={() => {}}>
+      <Modal open={true} setOpen={mockSetOpen}>
         <div>Modal Content</div>
       </Modal>,
     );
 
     const modalContent = screen.getAllByText(/Modal Content/i);
-    expect(modalContent).toHaveLength(2);
+    expect(modalContent).toHaveLength(1);
   });
+
   it("renders the buttons", () => {
     render(
-      <Modal open={true} setOpen={() => {}}>
+      <Modal
+        open={true}
+        setOpen={mockSetOpen}
+        onClickConfirm={mockOnClickConfirm}
+        onClickCancel={mockOnClickCancel}
+      >
         <div>Modal Content</div>
       </Modal>,
     );
 
     const ConfirmationButton = screen.getAllByText(/Confirmar/i);
-    expect(ConfirmationButton).toHaveLength(2);
+    expect(ConfirmationButton).toHaveLength(1);
+
     const CancelButton = screen.getAllByText(/Cancelar/i);
-    expect(CancelButton).toHaveLength(2);
+    expect(CancelButton).toHaveLength(1);
+  });
+
+  it("calls handleClose when XMarkIcon is clicked", () => {
+    render(
+      <Modal open={true} setOpen={mockSetOpen}>
+        <div>Modal Content</div>
+      </Modal>,
+    );
+
+    fireEvent.click(screen.getByTestId("close-button"));
+
+    expect(mockSetOpen).toHaveBeenCalledWith(false);
+  });
+
+  it("calls onClickConfirm when Confirm Button is clicked", () => {
+    render(
+      <Modal open={true} setOpen={mockSetOpen} onClickConfirm={mockOnClickConfirm}>
+        <div>Modal Content</div>
+      </Modal>,
+    );
+
+    fireEvent.click(screen.getByText("Confirmar"));
+
+    expect(mockOnClickConfirm).toHaveBeenCalled();
+  });
+
+  it("calls onClickCancel when Cancel Button is clicked", () => {
+    render(
+      <Modal open={true} setOpen={mockSetOpen} onClickCancel={mockOnClickCancel}>
+        <div>Modal Content</div>
+      </Modal>,
+    );
+
+    fireEvent.click(screen.getByText("Cancelar"));
+
+    expect(mockOnClickCancel).toHaveBeenCalled();
   });
 });
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,18 +1,16 @@
-import "react-spring-bottom-sheet/dist/style.css";
-
 import { XMarkIcon } from "@heroicons/react/24/solid";
+import { cva, VariantProps } from "class-variance-authority";
 import React, { ReactNode } from "react";
-import { BottomSheet } from "react-spring-bottom-sheet";
+import { twMerge } from "tailwind-merge";
 
 import Button, { ButtonProps } from "../Button";
 
-export interface ModalProps {
+export interface IModalProps {
   confirmLabel?: string;
   cancelLabel?: string;
   children?: ReactNode;
   onClickCancel?: () => void;
   onClickConfirm?: () => void;
-  open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   cancelOutline?: boolean;
   confirmOutline?: boolean;
@@ -20,10 +18,63 @@ export interface ModalProps {
   error?: boolean;
   loading?: boolean;
   buttons?: boolean;
-  onClose?: () => void;
+  handleClose?: () => void;
+  className?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({
+const ModalVariants = cva(
+  "fixed left-1/2 top-1/2 z-[100] h-auto min-w-[40rem] max-w-[52rem] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white max-md:hidden",
+  {
+    variants: {
+      open: {
+        true: "",
+        false: "hidden",
+      },
+    },
+  },
+);
+
+const BgVariants = cva(
+  "fixed right-0 top-0 z-50 h-full w-screen bg-gray-500 opacity-40 max-md:hidden",
+  {
+    variants: {
+      open: {
+        true: "",
+        false: "hidden",
+      },
+    },
+  },
+);
+
+const CancelButtonVariants = cva("w-full rounded-md border py-3", {
+  variants: {
+    error: {
+      true: "border-gray-400 bg-white text-gray-400 hover:bg-gray-300",
+      false: "text-primary-500",
+    },
+    cancelOutline: {
+      true: "btn-outline",
+    },
+  },
+});
+
+const ConfirmButtonVariants = cva("w-full rounded-md py-3 text-white", {
+  variants: {
+    error: {
+      true: "bg-error-500 hover:bg-error-400",
+      false: "bg-primary-500",
+    },
+    confirmOutline: {
+      true: "btn-outline",
+    },
+  },
+});
+
+type ModalVariantProps = VariantProps<typeof ModalVariants>;
+
+export interface ModalProps extends ModalVariantProps, IModalProps {}
+
+const Modal = ({
   confirmLabel = "Confirmar",
   cancelLabel = "Cancelar",
   children,
@@ -37,45 +88,25 @@ const Modal: React.FC<ModalProps> = ({
   error,
   loading = false,
   buttons = true,
-  onClose,
+  className,
 }: ModalProps) => {
   const handleClose = () => {
-    setOpen(false);
-    if (onClose) {
-      onClose();
-    }
+    setOpen(!open);
   };
-  const handleDismiss = () => {
-    handleClose();
-  };
-
   return (
     <>
       <div
-        className={`fixed right-0 top-0 z-50 h-full w-screen bg-gray-500 opacity-40 max-md:hidden ${
-          !open && "hidden"
-        }`}
-        onClick={() => {
-          setOpen(!open);
-          if (onClose) {
-            onClose();
-          }
-        }}
+        className={` 
+          ${twMerge(BgVariants({ open }))}
+        `}
+        onClick={handleClose}
       />
-      <div
-        className={`fixed left-1/2 top-1/2 z-[100] h-auto min-w-[40rem] max-w-[52rem] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white max-md:hidden ${
-          !open && "hidden"
-        }`}
-      >
+      <div className={`${twMerge(ModalVariants({ open }), className)}`}>
         <div className="relative z-50 h-full w-full gap-12 p-5">
           <XMarkIcon
+            data-testid="close-button"
             className="absolute right-4 top-4 h-6 w-6 cursor-pointer text-gray-500"
-            onClick={() => {
-              setOpen(!open);
-              if (onClose) {
-                onClose();
-              }
-            }}
+            onClick={handleClose}
           />
           <div className="h-fit">{children}</div>
           {buttons && (
@@ -83,17 +114,11 @@ const Modal: React.FC<ModalProps> = ({
               <Button
                 label={cancelLabel}
                 onClick={onClickCancel}
-                className={`w-full rounded-md border ${
-                  error
-                    ? "border-gray-400 bg-white text-gray-400 hover:bg-gray-300"
-                    : "text-primary-500"
-                } py-3 ${cancelOutline && "btn-outline"} ${!cancelLabel && "hidden"}`}
+                className={`${twMerge(CancelButtonVariants({ error, cancelOutline }))} ${!cancelLabel && "hidden"}`}
               />
               <Button
                 label={confirmLabel}
-                className={`w-full rounded-md ${
-                  error ? "bg-error-500 hover:bg-error-400" : "bg-primary-500"
-                } py-3 text-white ${confirmOutline && "btn-outline"} ${!confirmLabel && "hidden"}`}
+                className={`${twMerge(ConfirmButtonVariants({ error, confirmOutline }))} ${!confirmLabel && "hidden"}`}
                 onClick={onClickConfirm}
                 iconSide="left"
                 disabled={loading}
@@ -103,41 +128,6 @@ const Modal: React.FC<ModalProps> = ({
           )}
         </div>
       </div>
-      <BottomSheet
-        className="z-50 bg-white md:hidden"
-        open={open}
-        onDismiss={handleDismiss}
-        snapPoints={({ maxHeight }) => [maxHeight * 0.85]}
-      >
-        <div className="flex flex-col items-center gap-4 p-8">
-          <div className="h-fit min-h-[25rem]">{children}</div>
-          <div className="mt-auto w-full">
-            {buttons && (
-              <div className="absolute bottom-8 flex w-[85%] flex-col gap-2 max-md:p-1">
-                <Button
-                  label={cancelLabel}
-                  onClick={onClickCancel}
-                  className={`btn-outline w-full rounded-md border border-primary-500 py-3 text-primary-500 ${
-                    error
-                      ? "border-gray-400 bg-white text-gray-400 hover:bg-gray-300"
-                      : "text-primary-500"
-                  } ${!cancelLabel && "hidden"}`}
-                />
-                <Button
-                  label={confirmLabel}
-                  className={`w-full rounded-md py-3 text-white ${
-                    error ? "bg-error-500 hover:bg-error-400" : "bg-primary-500"
-                  } ${!confirmLabel && "hidden"}`}
-                  iconSide="left"
-                  icon={iconConfirm && iconConfirm}
-                  onClick={onClickConfirm}
-                  disabled={loading}
-                />
-              </div>
-            )}
-          </div>
-        </div>
-      </BottomSheet>
     </>
   );
 };
