33c114cb191ad2568e0b6342c6ebaeb891967780
Victor Hugo da Silva de Oliveira <vhvictor1@pop-os.localdomain>
2024-04-16 17:41:30 -0300

    🎨 Change the code with new reviews

--- src/components/Tab2.0/TabPanels.tsx
+++
@@ -1,9 +0,0 @@
-import { Tab } from "@headlessui/react";
-import { PropsWithChildren } from "react";
-
-const TabPanels = ({ children }: PropsWithChildren) => {
-  return <Tab.Panels>{children}</Tab.Panels>;
-};
-
-TabPanels.displyName = "Tab.Panels";
-export default TabPanels;
