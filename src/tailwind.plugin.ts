1cd17765a301b03f8afdf5cda246bc7d9dbc6cab
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-06-28 08:39:45 -0300

    set default colors for components

--- src/tailwind.plugin.ts
+++ src/tailwind.plugin.ts
@@ -10,67 +10,67 @@ const switchUiPlugin = plugin(function ({ addComponents, theme }) {
       borderRadius: theme("curvature.full", "9999px"),
     },
     ".btn-primary": {
-      backgroundColor: theme("colors.btn.primary.bg"),
-      color: theme("colors.btn.primary.text"),
+      backgroundColor: theme("colors.btn.primary.bg", "colors.primary.300"),
+      color: theme("colors.btn.primary.text", "colors.white"),
       "&:hover": {
-        backgroundColor: theme("colors.btn.primary.hover"),
+        backgroundColor: theme("colors.btn.primary.hover", "colors.primary.400"),
       },
       "&:focus": {
-        backgroundColor: theme("colors.btn.primary.focus"),
+        backgroundColor: theme("colors.btn.primary.focus", "colors.primary.300"),
       },
       "&:active": {
-        backgroundColor: theme("colors.btn.primary.active"),
+        backgroundColor: theme("colors.btn.primary.active", "colors.primary.500"),
       },
     },
     ".btn-outline": {
-      backgroundColor: theme("colors.btn.outline.bg"),
-      color: theme("colors.btn.outline.text"),
+      backgroundColor: theme("colors.btn.outline.bg", "transparent"),
+      color: theme("colors.btn.outline.text", "colors.primary.300"),
       "&:hover": {
-        backgroundColor: theme("colors.btn.outline.hover"),
+        backgroundColor: theme("colors.btn.outline.hover", "colors.primary.25"),
       },
       "&:focus": {
-        backgroundColor: theme("colors.btn.outline.focus"),
+        backgroundColor: theme("colors.btn.outline.focus", "colors.white"),
       },
       "&:active": {
-        backgroundColor: theme("colors.btn.outline.active"),
+        backgroundColor: theme("colors.btn.outline.active", "colors.primary.100"),
       },
     },
     ".btn-invisible": {
-      backgroundColor: theme("colors.btn.invisible.bg"),
-      color: theme("colors.btn.invisible.text"),
+      backgroundColor: theme("colors.btn.invisible.bg", "transparent"),
+      color: theme("colors.btn.invisible.text", "colors.gray.800"),
       "&:hover": {
-        backgroundColor: theme("colors.btn.invisible.hover"),
+        backgroundColor: theme("colors.btn.invisible.hover", "colors.gray.50"),
       },
       "&:focus": {
-        backgroundColor: theme("colors.btn.invisible.focus"),
+        backgroundColor: theme("colors.btn.invisible.focus", "colors.gray.white"),
       },
       "&:active": {
-        backgroundColor: theme("colors.btn.invisible.active"),
+        backgroundColor: theme("colors.btn.invisible.active", "colors.gray.100"),
       },
     },
     ".avatar-primary": {
-      backgroundColor: theme("colors.avatar.primary.bg"),
-      color: theme("colors.avatar.primary.text"),
+      backgroundColor: theme("colors.avatar.primary.bg", "colors.primary.50"),
+      color: theme("colors.avatar.primary.text", "colors.primary.400"),
     },
     ".avatar-gray": {
-      backgroundColor: theme("colors.avatar.gray.bg"),
-      color: theme("colors.avatar.gray.text"),
+      backgroundColor: theme("colors.avatar.gray.bg", "colors.gray.300"),
+      color: theme("colors.avatar.gray.text", "colors.gray.600"),
     },
     ".tab": {
-      borderBottomColor: theme("colors.tab.border"),
-      color: theme("colors.tab.text"),
+      borderBottomColor: theme("colors.tab.border", "colors.gray.500"),
+      color: theme("colors.tab.text", "colors.gray.500"),
 
       // Uses headlessui
       "&[data-headlessui-state~=selected]": {
-        borderBottomColor: theme("colors.tab.selectedBorder"),
-        color: theme("colors.tab.selectedText"),
+        borderBottomColor: theme("colors.tab.selectedBorder", "colors.primary.300"),
+        color: theme("colors.tab.selectedText", "colors.primary.300"),
       },
     },
     ".slider": {
-      backgroundColor: theme("colors.slider.bg"),
-      accentColor: theme("colors.slider.accent"),
+      backgroundColor: theme("colors.slider.bg", "colors.primary.300"),
+      accentColor: theme("colors.slider.accent", "colors.primary.300"),
       "&:hover": {
-        accentColor: theme("colors.slider.hover"),
+        accentColor: theme("colors.slider.hover", "colors.primary.400"),
       },
     },
     ".checkbox-primary": {
@@ -83,9 +83,9 @@ const switchUiPlugin = plugin(function ({ addComponents, theme }) {
       },
     },
     ".input": {
-      caretColor: theme("colors.input.caret"),
+      caretColor: theme("colors.input.caret", "colors.primary.100"),
       "&:focus": {
-        borderColor: theme("colors.input.focus"),
+        borderColor: theme("colors.input.focus", "colors.primary.100"),
       },
     },
   });
