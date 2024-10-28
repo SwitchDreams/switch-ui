6beb2163b7f4db3ad12000358fb26a008d19711b
Arthur Mota Furtado <chamatico2@gmail.com>
2024-10-28 08:44:04 -0300

    cleaning up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,92 +1,95 @@
-import react from "eslint-plugin-react";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import reactHooks from "eslint-plugin-react-hooks";
-import prettier from "eslint-plugin-prettier";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import treeShaking from "eslint-plugin-tree-shaking";
-import { fixupPluginRules } from "@eslint/compat";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+
+import { fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:storybook/recommended",
     "plugin:tailwindcss/recommended",
     "plugin:prettier/recommended",
-), {
+  ),
+  {
     plugins: {
-        react,
-        "@typescript-eslint": typescriptEslint,
-        "react-hooks": fixupPluginRules(reactHooks),
-        prettier,
-        "simple-import-sort": simpleImportSort,
+      react,
+      "@typescript-eslint": typescriptEslint,
+      "react-hooks": fixupPluginRules(reactHooks),
+      prettier,
+      "simple-import-sort": simpleImportSort,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
 
-        parserOptions: {
-            ecmaFeatures: {
-                tsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          tsx: true,
         },
+      },
     },
 
     settings: {
-        "import/resolver": {
-            typescript: {},
-        },
+      "import/resolver": {
+        typescript: {},
+      },
 
-        react: {
-            pragma: "React",
-            version: "detect",
-        },
+      react: {
+        pragma: "React",
+        version: "detect",
+      },
     },
 
     rules: {
-
-        camelcase: "error",
-        "no-duplicate-imports": "error",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "react/react-in-jsx-scope": "off",
-        "no-console": "error",
-        "no-alert": "error",
-        "react-hooks/exhaustive-deps": "off",
-        "react/prop-types": 0,
-        "react/display-name": 0,
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        "@typescript-eslint/no-empty-function": "off",
-        "react/no-unknown-property": "off",
-        "react/no-unescaped-entities ": "off",
-        "tailwindcss/no-custom-classname": ["off"],
-        "tailwindcss/enforces-shorthand": "off",
+      camelcase: "error",
+      "no-duplicate-imports": "error",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-console": "error",
+      "no-alert": "error",
+      "react-hooks/exhaustive-deps": "off",
+      "react/prop-types": 0,
+      "react/display-name": 0,
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-empty-function": "off",
+      "react/no-unknown-property": "off",
+      "react/no-unescaped-entities ": "off",
+      "tailwindcss/no-custom-classname": ["off"],
+      "tailwindcss/enforces-shorthand": "off",
     },
-}, {
+  },
+  {
     files: ["**/*.stories.tsx", "**/*.test.tsx", "**/*.spec.tsx"],
 
     rules: {
-        "tree-shaking/no-side-effects-in-initialization": "off",
+      "tree-shaking/no-side-effects-in-initialization": "off",
     },
-}];
+  },
+];
